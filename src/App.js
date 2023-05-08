272ed3c1702d65c5ee6e6e7c98de6da3c48ee421
AugustoCavalcanti <cardoso0guto@gmail.com>
2023-05-07 23:21:32 -0300

    feat: Adiciona configuração das rotas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './App.css';
 import Capa from './components/Capa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CaroselPerguntas from './components/CaroselPerguntas';
-import Resultado from './components/Resultado';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Resultado from './components/Resultado';
+import Home from './pages/Home';
+import Carosel from './pages/Carosel';
+import Resultado from './pages/Resultado';
 
 function App() {
   return (
-    <Resultado />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="perguntas" element={<Carosel />} />
+          <Route path="resultado" element={<Resultado />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+// function App() {
+//   return (
+//     <Resultado />
+//   );
+// }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+export default function Home() {
+    return <>
+        <section>
+            <div className="container">
+                <div className="row  h-100 align-items-center">
+                    <div className="col-12 d-flex justify-content-center align-items-center">
+                        <div className="aling-middle mb-5" id="capa">
+                            <img className="logo my-5" src="https://images.vexels.com/media/users/3/140278/isolated/lists/73d2c1d8a07fb1705bcad0712f07ec34-planta-floral.png" />
+                            <h3>Id Plant</h3>
+                            <p>Que tal testar os seus conhecimentos sobre a fauna local ?</p>
+                            <p>Vamos descobrir qual o nível da sua segueira vegetal</p>
+                            <button type="button" class="btn btn-outline-success align-self-center">Começar <i class="bi bi-arrow-right-short"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+}
