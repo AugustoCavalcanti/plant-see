0c37fac7988f60cb6ff8aa06f38fbebb456edbae
AugustoCavalcanti <cardoso0guto@gmail.com>
2023-05-18 18:56:59 -0300

    feat: Altera o template de resultado para exibir os resultados retornados pela api.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,9 @@ export default function Home() {
                 <div className="row  h-100 align-items-center">
                     <div className="col-12 d-flex justify-content-center align-items-center">
                         <div className="aling-middle mb-5" id="capa">
-                            <img className="logo my-5" src="https://images.vexels.com/media/users/3/140278/isolated/lists/73d2c1d8a07fb1705bcad0712f07ec34-planta-floral.png" />
-                            <h3>Id Plant</h3>
+                            <img className="logo my-5" src="/logo-1.png" />
                             <p>Que tal testar os seus conhecimentos sobre a fauna local ?</p>
-                            <p>Vamos descobrir qual o nível da sua segueira vegetal</p>
+                            <p>Vamos descobrir qual o nível da sua segueira vegetal ?</p>
                             <Link type="button" className="btn btn-outline-success align-self-center" to="/perguntas">Começar <i className="bi bi-arrow-right-short"></i></Link>
                         </div>
                     </div>
--- src/pages/Resultado.js
+++ src/pages/Resultado.js
@@ -6,7 +6,7 @@ export default function Resultado() {
     const { id } = useParams();
     const [resposta, setResposta] = useState([])
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/answer/' + id + '')
+        fetch('http://127.0.0.1:8000/api/answer/' + id + '/')
             .then(res => res.json())
             .then(res => {
                 setResposta(res)
@@ -19,11 +19,11 @@ export default function Resultado() {
                 <div className="row  h-100 align-items-center">
                     <div className="col-12 d-flex justify-content-center align-items-center">
                         <div className="aling-middle mb-5" id="capa">
-                            <img className="logo my-5" src="https://images.vexels.com/media/users/3/140278/isolated/lists/73d2c1d8a07fb1705bcad0712f07ec34-planta-floral.png" />
+                            <img className="logo my-5" src="/logo-1.png" />
                             <h3>Id Plant</h3>
                             <p>Você acertou:</p>
-                            <h5>7/10</h5>
-                            <p>Ótimo resultado!</p>
+                            <h5>{resposta.acertos}/10</h5>
+                            <p>Você acertou mais que {resposta.porcentagem}% das pessoas que já responderam !</p>
                             <Link type="button" className="btn btn-outline-success align-self-center" to="/">Refazer ! <i className="bi bi-arrow-right-short"></i></Link>
                         </div>
                     </div>
